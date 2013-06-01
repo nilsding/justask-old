f1c1ebe5e455ab3dbb90fa67620dc782fad37860
nilsding <nilsding@nilsding.org>
2013-06-01 09:24:41 +0200

    hopefully fixed a small bug when used with IIS (why am I even doing this)

--- ask.php
+++ ask.php
@@ -70,7 +70,7 @@ if (strlen($question_asked_by) > 100) {
 }
 
 
-$sql_str = 'INSERT INTO `' . $MYSQL_TABLE_PREFIX . 'inbox` (`question_content`, `asker_name`, `asker_gravatar`, `asker_private`) VALUES (\'' . $question . '\', \'' . $question_asked_by . '\', \'' . $gravatar_address . '\', \'' . $question_asked_anonymously . '\')';
+$sql_str = 'INSERT INTO `' . $MYSQL_TABLE_PREFIX . 'inbox` (`question_content`, `asker_name`, `asker_gravatar`, `asker_private`) VALUES (\'' . $question . '\', \'' . $question_asked_by . '\', \'' . $gravatar_address . '\', \'' . ($question_asked_anonymously ? '1' : '0') . '\')';
 
 if (!$sql->query($sql_str)) {
   echo "<p>The query <code>$sql_str</code> failed! :(</p>";
