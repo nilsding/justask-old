0eef3defc71dfedc2594b6af7178850559d2e2a7
MeikoDis <meikodis@meikodis.org>
2013-06-24 10:47:19 +0200

    Try somethin

--- fixDir.php
+++ fixDir.php
@@ -4,9 +4,10 @@
     if(stristr($scrpt,"/")){
       $arSc=explode("/", $scrpt);
       $scNumb=count($arSc);
+
       for($i=0; $i<$scNumb-1; $i++){
         $newDir.=$arSc[$i];
-	if($i!=($scNumb-1)) $newDir.="/";
+	if($i<($scNumb-2)) $newDir.="/";
       }
 
       return $newDir;
